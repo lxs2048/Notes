9acdc60bde08ffb2f2a3435915627926861b9522
zhangsan <lxs2048@sina.com>
2022-11-23 15:11:57 +0800

    调整lodash预览样式

--- src/pages/preview/index.tsx
+++ src/pages/preview/index.tsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import Layout from '@theme/Layout';
 import lodashJson from './lodash221121.json'
+import styles from './preview.module.scss'
+interface LodashMethod {
+    [key: string]: string,
+};
 function Preview() {
+    const baseUrl = 'https://www.lodashjs.com/docs/lodash.'
+    const lists:LodashMethod = lodashJson
     return (
-        <Layout title="Hello" description="Hello React Page">
-            <div className='preview-content'>
-            {
-                Object.keys(lodashJson).map(item=>{
-                    return <div key={item}>
-                        {lodashJson[item]}
-                    </div>
-                })
-            }
-        </div>
+        <Layout title="预览" description="">
+            <div className={`${styles['preview-container']}`}>
+                <table className="table-fixed">
+                    <thead>
+                        <tr>
+                            <th>lodash方法</th>
+                            <th>描述</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            Object.keys(lists).map(item => {
+                                return <tr key={item}>
+                                    <td><a target='_black' href={`${baseUrl}${item}`}>{item}</a></td>
+                                    <td>{lists[item]}</td>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
         </Layout>
     )
 }
